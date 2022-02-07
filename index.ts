a70cada0254adb523e7562611f1b9352e2f90d2c
DiyaBaliga <baligadiya@gmail.com>
2022-02-06 20:14:09 -0800

    Refactored port declarations

--- index.ts
+++ index.ts
@@ -5,12 +5,15 @@ const airtableConfig = {
   baseKey: process.env.REACT_APP_AIRTABLE_BASE_KEY,
 };
 
+const clientPort = 3000;
+const serverPort = 8000;
+
 new Airlock({
   airtableApiKey: [airtableConfig.apiKey],
   airtableBaseId: airtableConfig.baseKey,
   airtableUserTableName: "Users",
   airtableUsernameColumn: "username",
   airtablePasswordColumn: "password",
-  allowedOrigins: ['http://localhost:8000'],
-  port: 3000,
+  allowedOrigins: ['http://localhost:${clientPort}'],
+  port: serverPort,
 });
