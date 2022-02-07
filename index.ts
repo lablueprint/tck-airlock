656ee230927564aa935118a5f2053a1bcc9f0b59
DiyaBaliga <baligadiya@gmail.com>
2022-02-06 20:10:51 -0800

    Setup airlock config, ports and fixed dev script
    The dev script causes errors as is. This might be an issue
    with Windows compatibility, so this fix should also be tested on
    Macs.

--- index.ts
+++ index.ts
@@ -1,11 +1,16 @@
 import Airlock from "airlock-server";
 
+const airtableConfig = {
+  apiKey: process.env.REACT_APP_AIRTABLE_USER_KEY,
+  baseKey: process.env.REACT_APP_AIRTABLE_BASE_KEY,
+};
+
 new Airlock({
-  airtableApiKey: [],
-  airtableBaseId: "",
-  airtableUserTableName: "",
-  airtableUsernameColumn: "",
-  airtablePasswordColumn: "",
-  allowedOrigins: [],
+  airtableApiKey: [airtableConfig.apiKey],
+  airtableBaseId: airtableConfig.baseKey,
+  airtableUserTableName: "Users",
+  airtableUsernameColumn: "username",
+  airtablePasswordColumn: "password",
+  allowedOrigins: ['http://localhost:8000'],
   port: 3000,
 });
